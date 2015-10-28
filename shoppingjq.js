1502d6a11cb28118e6778efd888037e180ab9509
LukeBucciarelli <luke.bucciarelli@gmail.com>
2015-10-28 14:56:35 -0400

    2nd push

---
+++ shoppingjq.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){ 
+
+
+$('need').on('click', 'div', function(event){
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+});
+
+
+
+
+
+
+
+
+}
