680970f833f155c164823074dce781c71a304e7e
fredericJos <fredericaerts@hotmail.com>
2015-11-03 19:39:17 +0100

    session 3rd novmeber

--- shoppingjq.js
+++ shoppingjq.js
@@ -9,17 +9,39 @@ $(document).ready(function(){
 /*	end mentor JS code  
 	========================== */
 
+	$('.addtolist').on('click', function(event){
+		// fetching input value
+		var todoString = $('.inputitem').val();
+
+		// fetching inout value
+		var $listItem = $('<li></li>');
+		$listItem.addClass('shopping-list__item');
+
+		var $doneButton = $('<button></button>');
+		$doneButton.addClass('shopping-list__item__button');
+		$doneButton.addClass('js-check-item-button');
+		$doneButton.text('Done');
+		$doneButton.on('click', function(event){
+			$(this).parent().toggleClass('done');
+		});
+
+		$listItem.text(todoString);
+		$listItem.append($doneButton);
+
+		$('.shopping-list').append($listItem);
+
+		$('.inputitem').val('');
+	});
+
 // $('.done').on('click', function(event){
 
 // 	$(this).parent().removeClass('need')
 // 	.addClass('got');
 // });
 
-$('.addtolist').on('click', function(event){
-
-
-console.log($('.inputitem').text());
-});
+// $('.addtolist').on('click', function(event){
+// 	console.log($('.inputitem').val());
+// });
 
 
 } ); 
